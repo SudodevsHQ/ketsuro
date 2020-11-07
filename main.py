6183956c1bb74648b58e1135368bfcc2a59a52c3
AayushDongre <aayushdongre4@gmail.com>
2020-11-07 14:34:00 +0530

    feat: ytl download video

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from youtube_transcript_api import YouTubeTranscriptApi
+import youtube_dl
+import json
 
 from utils import punctuate
 from models.PunctuatedResponse import PunctuatedTranscript
@@ -11,9 +13,21 @@ def get_video(video_id: str):
     data = YouTubeTranscriptApi.get_transcript(video_id)
     raw_transcript = ' '
     raw_transcript = raw_transcript.join([f"{info['text']}" for info in data])
-
     response = punctuate(raw_transcript)
-    return response.text
+    response = json.loads(response.text)
+
+    raw_text = '\n'
+    raw_text = raw_text.join([f"{info['start']}: {info['text']}" for info in data])
+    print(raw_text)
+
+    ytdl_opts = {
+        'outtmpl': f"videos/{response['request_id']}",
+        'nooverwrites': True
+    }
+    with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
+        ydl.download([f'http://www.youtube.com/watch?v={video_id}'])
+
+    return response
 
 @app.post('/webhook')
 def getPunctuate(item: PunctuatedTranscript):
--- test.py
+++ test.py
@@ -11,18 +11,18 @@ from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
 LANGUAGE = "english"
-SENTENCES_COUNT = 1
-vid_id = '5tQLFon8Yhk'
+SENTENCES_COUNT = 3
+# vid_id = '5tQLFon8Yhk'
 
-data = YouTubeTranscriptApi.get_transcript(vid_id)
+# data = YouTubeTranscriptApi.get_transcript(vid_id)
 
-raw_text = ' '
-raw_text = raw_text.join([f"{info['start']}: {info['text']}" for info in data])
-print(raw_text)
+# raw_text = ' '
+# raw_text = raw_text.join([f"{info['start']}: {info['text']}" for info in data])
+# print(raw_text)
 
 if __name__ == "__main__":
 
-    parser = PlaintextParser.from_string('In an attempt to build an AI-ready workforce, Microsoft announced Intelligent Cloud Hub which has been launched to empower the next generation of students with AI-ready skills. Envisioned as a three-year collaborative program, Intelligent Cloud Hub will support around 100 institutions with AI infrastructure, course content and curriculum, developer support, development tools and give students access to cloud and AI services. As part of the program, the Redmond giant which wants to expand its reach and is planning to build a strong developer ecosystem in India with the program will set up the core AI infrastructure and IoT Hub for the selected campuses. The company will provide AI development tools and Azure AI services such as Microsoft Cognitive Services, Bot Services and Azure Machine Learning.According to Manish Prakash, Country General Manager-PS, Health and Education, Microsoft India, said, "With AI being the defining technology of our time, it is transforming lives and industry and the jobs of tomorrow will require a different skillset. This will require more collaborations and training and working with AI. That’s why it has become more critical than ever for educational institutions to integrate new cloud and AI technologies. The program is an attempt to ramp up the institutional set-up and build capabilities among the educators to educate the workforce of tomorrow." The program aims to build up the cognitive skills and in-depth understanding of developing intelligent cloud connected solutions for applications across industry. Earlier in April this year, the company announced Microsoft Professional Program In AI as a learning track open to the public. The program was developed to provide job ready skills to programmers who wanted to hone their skills in AI and data science with a series of online courses which featured hands-on labs and expert instructors as well. This program also included developer-focused AI school that provided a bunch of assets to help build AI skills.', Tokenizer(LANGUAGE))
+    parser = PlaintextParser.from_string('''Oh, no, a the it's not, it's it. There's no need even to have a college degree at all. We're in high school, but I mean, if somebody graduated from a great university that maybe in indeed, that may be an indication that they will be capable of great things, but it's not necessarily the case. You know, if you look at say people like Bill Gates or Larry Ellison, Steve Jobs.  These guys didn't graduate from college, but if you had a chance to hire them.  Of course, that would be a good idea, so you know, just looking just for evidence of exceptional ability, and if there's a track record of exceptional achievement, then it's likely that that will continue into the future.''', Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
 
     summarizer = Summarizer(stemmer)
