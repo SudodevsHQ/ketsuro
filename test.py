ea08e0ee9c37e44ced56547823797d6f34c0008a
therexone <renerjake@gmail.com>
2020-11-07 13:21:56 +0530

    deepaffects api test

--- test.py
+++ test.py
@@ -10,29 +10,28 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.luhn import LuhnSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
-import os
 
 LANGUAGE = "english"
 SENTENCES_COUNT = 5
-vid_id = '8gNTyr1DXc4'
+vid_id = '0WVpW497vTU'
 
 
 data = YouTubeTranscriptApi.get_transcript(vid_id)
 raw_text = ' '.join([info['text'] for info in data])
+print(raw_text)
 
-p = Punctuator('models/INTERSPEECH-T-BRNN.pcl')
-punc_text = p.punctuate(raw_text)
+# p = Punctuator('models/INTERSPEECH-T-BRNN.pcl')
+# punc_text = p.punctuate(raw_text)
 
 
-print('-------------------------------------\n')
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    parser = PlaintextParser.from_string(punc_text, Tokenizer(LANGUAGE))
-    stemmer = Stemmer(LANGUAGE)
+#     parser = PlaintextParser.from_string(punc_text, Tokenizer(LANGUAGE))
+#     stemmer = Stemmer(LANGUAGE)
 
-    summarizer = Summarizer(stemmer)
-    summarizer.stop_words = get_stop_words(LANGUAGE)
+#     summarizer = Summarizer(stemmer)
+#     summarizer.stop_words = get_stop_words(LANGUAGE)
 
-    for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        print(sentence)
+#     for sentence in summarizer(parser.document, SENTENCES_COUNT):
+#         print(sentence)
